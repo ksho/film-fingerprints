def2df3baea140632acf6d62fe4f676660b05f93
Karl Shouler <karl.shouler@gmail.com>
2012-05-30 17:51:50 -0400

    cleanup

--- test.py
+++ test.py
@@ -1,76 +1,72 @@
 import pyffmpeg as pf
 from PIL import Image
+import math
+
+
+"""
+TODO:
+- multithreading
+- banner
+"""
 
 FRAME_SIZE = {}
-# bars = []
 
-def get_bar(f, i):
-    x = FRAME_SIZE['x'] / 2
+
+def get_bar_from_frame(f, i):
+    x = FRAME_SIZE['xmid']
     y = FRAME_SIZE['y']
-    f = f.crop((x, 0, x + 1, y))
+    #f = f.crop((x, 0, x + 1, y))
+    f = f.resize((1, FRAME_SIZE['y']))
     return f
 
 
-# def build_composite():
-#     num_bars = len(bars)
-#     print 'num_bars', num_bars, FRAME_SIZE
-#     composite = Image.new('RGB',(num_bars, FRAME_SIZE['y']),(255,255,255))
-#     for k in xrange(0, num_bars):
-#         composite.paste(bars[k], (k,0))
-# #    composite = composite.resize((composite.size[0], 70))
-#     return composite
-
-
-def get_frame_size(tv):
-    frame = tv.get_current_frame()[2]
-    print frame.size
-    FRAME_SIZE['x'] = frame.size[0]
-    FRAME_SIZE['y'] = frame.size[1]
+def get_frame_size(video_track):
+    #frame = video_track.get_current_frame()[2]
+    size = video_track.get_size()
+    FRAME_SIZE['x'] = size[0]
+    FRAME_SIZE['y'] = size[1]
+    FRAME_SIZE['xmid'] = FRAME_SIZE['x'] / 2
     print FRAME_SIZE
 
 
 # Create reader object
-mp = pf.FFMpegReader()
+reader = pf.FFMpegReader()
 
 # Open an audio-video file
-mp.open("/home/karlshouler/Desktop/how.mkv", pf.TS_VIDEO_PIL)
-tv = mp.get_tracks()[0]
+reader.open("/home/karlshouler/Desktop/how.mkv", pf.TS_VIDEO_PIL)
 
-duration = mp.duration_time()
-tv = mp.get_tracks()[0]
-fps = tv.get_fps()
-num_frames = int(fps * duration)
+video_track = reader.get_tracks()[0]
+fps = video_track.get_fps()
+duration = reader.duration_time()
+num_frames = math.floor(fps * duration) - 10
 print 'fps', fps, duration, num_frames
-get_frame_size(tv)
 
-num_frames = 100
+get_frame_size(video_track)
+
+#num_frames = 10000
+
+#video_track.seek_to_frame(30000)
+#num_frames = num_frames - 30000
+#print num_frames
 
 composite = Image.new('RGB', (num_frames, FRAME_SIZE['y']), (255, 255, 255))
+video_track.prepare_to_read_ahead()
 
 i = 0
+frame = video_track.get_current_frame()[2]
+bar = get_bar_from_frame(frame, i)
+composite.paste(bar, (i, 0))
+
+i += 1
 while i < num_frames:
-    try:
-        f = tv.get_next_frame()
-        b = get_bar(f, i)
-        composite.paste(b, (i, 0))
-        i = i + 1
-        if i % 100 is 0:
-            print i, f, tv.get_current_frame_pts(), tv.get_current_frame_frameno()
-    except:
-        print 'lalala'
-        break
+    frame = video_track.get_next_frame()
+    bar = get_bar_from_frame(frame, i)
+    composite.paste(bar, (i, 0))
+    i += 1
+    if i % 1000 is 0:
+        print i, frame, video_track.get_current_frame_pts(), video_track.get_current_frame_frameno()
 
-composite.save('1111.png')
-
-# print i
-# tv.seek_to_frame(100000)
-# print 'fps1', tv.get_fps(), tv.get_current_frame_pts(), tv.get_current_frame_frameno()
-
-# frame = tv.get_current_frame()[2]
-
-#composite_image = build_composite()
-
-#composite_image.save('lala4.png')
+composite.save('2222.png')
 
 
 
