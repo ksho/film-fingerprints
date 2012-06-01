32f61f517c36eae42711df7227436b37c6468c0b
Karl Shouler <karl.shouler@gmail.com>
2012-06-01 08:59:02 -0400

    multicore

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pyffmpeg as pf
 from PIL import Image
 import math
 import threading
-import Queue
+from multiprocessing import Process, Queue
 
 
 """
@@ -13,7 +13,7 @@ TODO:
 
 
 FRAME_SIZE = {}
-
+HALVES = []
 
 def get_bar_from_frame(f, i):
     x = FRAME_SIZE['xmid']
@@ -32,8 +32,9 @@ def get_frame_size(video_track):
     print FRAME_SIZE
 
 def do_the_stuff(video_track, start, end, tnum):
-    print 'start', start, end
-    num_frames = end - start
+    
+    num_frames = int(end - start)
+    print 'start', start, end, num_frames
     video_track.seek_to_frame(start)
     composite = Image.new('RGB', (num_frames/10, FRAME_SIZE['y']), (255, 255, 255))
     video_track.prepare_to_read_ahead()
@@ -41,33 +42,38 @@ def do_the_stuff(video_track, start, end, tnum):
     i = 0
     frame = video_track.get_current_frame()[2]
     bar = get_bar_from_frame(frame, i)
-    composite.paste(bar, (i, 0))
+    composite.paste(bar, (i/10, 0))
 
-    i += 10
+    i += 1
     while i < num_frames:
         try:
-            video_track.seek_to_frame(i)
-            frame = video_track.get_current_frame()[2]
-            bar = get_bar_from_frame(frame, i)
-            composite.paste(bar, (i, 0))
-            i += 10
-            if i % 100 is 0:
-                print tnum, i, frame, video_track.get_current_frame_pts()
+            frame = video_track.get_next_frame()
+            if i % 10 is 0:
+                bar = get_bar_from_frame(frame, i)
+                composite.paste(bar, (i/10, 0))
+            i += 1
+            if i % 500 is 0:
+                print tnum, i, frame, video_track.get_current_frame_pts(), video_track.get_current_frame_frameno()
+            if i % 2000 is 0:
+                composite.save(str(tnum)+'444toy.png')
         except:
             break
     print 'stop'
-    q.put(composite)#composite
+    HALVES.append(composite)
+    composite.save(str(tnum)+'toy.png')
 
 
-q = Queue.Queue()
+
 
 # Create reader object
 reader = pf.FFMpegReader()
 reader2= pf.FFMpegReader()
 
+movie = "/media/stuff/bttf.mkv"
+
 # Open an audio-video file
-reader.open("/home/karlshouler/Desktop/bttf.mkv", pf.TS_VIDEO_PIL)
-reader2.open("/home/karlshouler/Desktop/bttf.mkv", pf.TS_VIDEO_PIL)
+reader.open(movie, pf.TS_VIDEO_PIL)
+reader2.open(movie, pf.TS_VIDEO_PIL)
 
 video_track = reader.get_tracks()[0]
 video_track2 = reader2.get_tracks()[0]
@@ -78,8 +84,7 @@ print 'fps', fps, duration, num_frames
 
 get_frame_size(video_track)
 
-#num_frames = 10000
-
+num_frames = 1000
 
 video_track.seek_to_frame(100)
 num_frames = num_frames - 100
@@ -90,18 +95,33 @@ start2 = num_frames / 2;
 end1 = start2 - 1
 end2 = num_frames
 
-t1 = threading.Thread(target=do_the_stuff, args=(video_track,start1,end1,1))
-t2 = threading.Thread(target=do_the_stuff, args=(video_track2,start2,end2,2))
-t1.start()
-t2.start()
+p1 = Process(target=do_the_stuff, args=[video_track,start1,end1,1])
+p2 = Process(target=do_the_stuff, args=[video_track2,start2,end2,2])
+p1.start()
+p2.start()
 
-composite = q.get()
-print composite
-composite2 = q.get()
-print composite2
+p1.join()
+p2.join()
 
-composite.save('333.png')
-composite2.save('444.png')
+
+print '!!!!!!!!!!!!!!!!!!!'
+# print q.get()
+# print composite
+# print q.get()
+# print composite2
+
+composite = Image.new('RGB', (num_frames/10, FRAME_SIZE['y']), (255, 255, 255))
+composite.paste(HALVES.pop(), (0, 0))
+composite.paste(HALVES.pop(), (start2, 0))
+
+composite.save('sup.png')
+
+
+
+
+
+# composite.save('333.png')
+# composite2.save('444.png')
 
 
 
