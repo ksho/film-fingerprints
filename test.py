1da2c80768245593cb5e2d10a433dc4717e18e4c
Karl Shouler <karl.shouler@gmail.com>
2012-06-05 14:53:58 -0400

    working multicore

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.6
+
 import pyffmpeg as pf
 from PIL import Image
 import math
@@ -7,19 +9,20 @@ from multiprocessing import Process, Queue
 
 """
 TODO:
-- multithreading
 - banner
 """
 
-
+FACTOR = 10
 FRAME_SIZE = {}
-HALVES = []
+VIDEO_LOC = "/home/karlshouler/Desktop/how.mkv"
+VIDEO_NAME = "how"
+NUM_PROCESSES = 2
 
 def get_bar_from_frame(f, i):
     x = FRAME_SIZE['xmid']
     y = FRAME_SIZE['y']
-    f = f.crop((x, 0, x + 1, y))
-    #f = f.resize((1, FRAME_SIZE['y']), Image.ANTIALIAS)
+    #f = f.crop((x, 0, x + 1, y))
+    f = f.resize((1, FRAME_SIZE['y']), Image.ANTIALIAS)
     return f
 
 
@@ -31,49 +34,56 @@ def get_frame_size(video_track):
     FRAME_SIZE['xmid'] = FRAME_SIZE['x'] / 2
     print FRAME_SIZE
 
-def do_the_stuff(video_track, start, end, tnum):
+
+def do_the_stuff(vt, start, end, tnum):
     
     num_frames = int(end - start)
-    print 'start', start, end, num_frames
-    video_track.seek_to_frame(start)
-    composite = Image.new('RGB', (num_frames/10, FRAME_SIZE['y']), (255, 255, 255))
-    video_track.prepare_to_read_ahead()
+    print 'start', tnum, start, end, num_frames
+    vt.seek_to_frame(start)
+    print 'seek complete', tnum
+    composite = Image.new('RGB', (num_frames/FACTOR, FRAME_SIZE['y']), (255, 255, 255))
+    vt.prepare_to_read_ahead()
 
     i = 0
-    frame = video_track.get_current_frame()[2]
+    frame = vt.get_current_frame()[2]
     bar = get_bar_from_frame(frame, i)
-    composite.paste(bar, (i/10, 0))
+    composite.paste(bar, (i/FACTOR, 0))
 
     i += 1
     while i < num_frames:
         try:
-            frame = video_track.get_next_frame()
-            if i % 10 is 0:
+            frame = vt.get_next_frame()
+            if i % FACTOR is 0:
                 bar = get_bar_from_frame(frame, i)
-                composite.paste(bar, (i/10, 0))
+                composite.paste(bar, (i/FACTOR, 0))
             i += 1
-            if i % 500 is 0:
-                print tnum, i, frame, video_track.get_current_frame_pts(), video_track.get_current_frame_frameno()
-            if i % 2000 is 0:
-                composite.save(str(tnum)+'444toy.png')
+            if i % 100 is 0:
+                print tnum, i, frame, vt.get_current_frame_pts(), vt.get_current_frame_frameno()
+            if i % 5000 is 0:
+                composite.save(VIDEO_NAME + 'part' + str(tnum) + '.png')
         except:
             break
     print 'stop'
-    HALVES.append(composite)
-    composite.save(str(tnum)+'toy.png')
+    image_str = composite.tostring()
+    q.put({'half': tnum, 'str': image_str, 'size': composite.size, 'mode': composite.mode})
+    composite.save(VIDEO_NAME + 'part' + str(tnum) + '.png')
 
 
+########################
+########################
 
+q = Queue()
 
+readers = []
 # Create reader object
 reader = pf.FFMpegReader()
 reader2= pf.FFMpegReader()
 
-movie = "/media/stuff/bttf.mkv"
-
 # Open an audio-video file
-reader.open(movie, pf.TS_VIDEO_PIL)
-reader2.open(movie, pf.TS_VIDEO_PIL)
+reader.open(VIDEO_LOC, pf.TS_VIDEO_PIL)
+reader2.open(VIDEO_LOC, pf.TS_VIDEO_PIL)
+
+
 
 video_track = reader.get_tracks()[0]
 video_track2 = reader2.get_tracks()[0]
@@ -84,14 +94,14 @@ print 'fps', fps, duration, num_frames
 
 get_frame_size(video_track)
 
-num_frames = 1000
+num_frames = 2000
 
 video_track.seek_to_frame(100)
 num_frames = num_frames - 100
 print num_frames
 
-start1 = 100
-start2 = num_frames / 2;
+start1 = 0
+start2 = int(num_frames / 2);
 end1 = start2 - 1
 end2 = num_frames
 
@@ -100,28 +110,29 @@ p2 = Process(target=do_the_stuff, args=[video_track2,start2,end2,2])
 p1.start()
 p2.start()
 
+
+print '!!!!!!!!!!!!!!!!!!!'
+halves = []
+halves.append(q.get())
+halves.append(q.get())
+
 p1.join()
 p2.join()
 
+for h in halves:
+    print 'lala'
+    if h['half'] is 1:
+        first = Image.fromstring(h['mode'], h['size'], h['str'])
+        print 's1', first.size
+    elif h['half'] is 2:
+        second = Image.fromstring(h['mode'], h['size'], h['str'])
+        print 's2', second.size
 
-print '!!!!!!!!!!!!!!!!!!!'
-# print q.get()
-# print composite
-# print q.get()
-# print composite2
+delta = first.size[0] - second.size[0]
 
-composite = Image.new('RGB', (num_frames/10, FRAME_SIZE['y']), (255, 255, 255))
-composite.paste(HALVES.pop(), (0, 0))
-composite.paste(HALVES.pop(), (start2, 0))
+
+composite = Image.new('RGB', (num_frames/FACTOR + delta, FRAME_SIZE['y']), (255, 255, 255))
+composite.paste(first, (0, 0))
+composite.paste(second, (num_frames/FACTOR/2 + delta, 0))
 
 composite.save('sup.png')
-
-
-
-
-
-# composite.save('333.png')
-# composite2.save('444.png')
-
-
-
